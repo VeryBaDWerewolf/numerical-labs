5095559671b0d4db4e5071c9b2712422d6676fed
VeryBadWerewolf <bad1werewolf@yandex.ru>
2023-09-23 16:01:47 +0700

    Standart vector arithmetic operations

--- utils/headers/vector.h
+++ utils/headers/vector.h
@@ -1,15 +1,32 @@
-#ifdef VECTOR_H
-#define VECTOR_H
-
+#ifndef VECTOR_H
+#define VECTOR_H 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <assert.h>
 typedef struct dvector dvector;
 
+struct dvector{
+  double *data;
+  size_t length;
+};
 
-void vector_initd(dvector *v, size_t size);
-int vector_set(dvector *v, double *d, size_t size);
-void vector_freed(dvector *v);
 
-dvector vector_sum(dvector *u, vector*v);
-dvector vector_dif_Nonnull(dvector *u, vector*v);
+void vector_init(dvector *v, size_t size);
+void vector_set(dvector *v, double *d, size_t size);
+void vector_free(dvector *v);
+
+
+dvector vector_negate(dvector *v);
+dvector vector_sum(dvector *u, dvector *v);
+dvector vector_dif(dvector *u, dvector *v);
+dvector vector_sc_mult(dvector *v, double sclr);
+
+
+
+double vector_dot(dvector *u, dvector *v);
+
+void vprint(dvector *v);
 
 
 #endif
--- utils/vector.c
+++ utils/vector.c
@@ -1,27 +1,19 @@
 #include "headers/vector.h"
-#include <stdlib.h>
-typedef struct dvector dvector;
-
-struct dvector{
-  double *data;
-  size_t length;
-};
-
 
 void vector_init(dvector *v, size_t size){
   v->length = size;
-  v->data = calloc(size, sizeof(void*));
+  v->data = calloc(size, sizeof(double));
 }
 
-int vector_set(dvector *v, double *d, size_t size){
-  if(size != v->length)
-    return 0;
+void vector_set(dvector *v, double *d, size_t size){
+  vector_init(v, size);
   for(int i = 0; i<size; i++)
     v->data[i] = d[i];
-  return size;
 }
 
 dvector vector_sum(dvector *u, dvector *v){
+  assert(u->length == v->length);
+  
   dvector sum;
   int l = u->length;
 
@@ -34,15 +26,53 @@ dvector vector_sum(dvector *u, dvector *v){
 }
 
 dvector vector_dif(dvector *u, dvector *v){
+  assert(u->length == v->length);
   dvector dif;
   int l = u->length;
 
   vector_init(&dif, l);
 
   for(int i = 0; i<l; i++)
-    dif.data[i] = u->data[i] + v->data[i];
+    dif.data[i] = u->data[i] - v->data[i];
 
   return dif; 
+}
+
+dvector vector_sc_mult(dvector *u, double sclr){
+  dvector scmlt;
+  int l = u->length;
+
+  vector_init(&scmlt, l);
+
+  for(int i = 0; i<l; i++)
+    scmlt.data[i] = u->data[i]*sclr;
+
+  return scmlt; 
+}
+
+dvector vector_negate(dvector *v){
+  dvector nv;
+  vector_init(&nv, v->length);
+  nv = vector_dif(&nv, v);
+  return nv;
+}
+
+
+double vector_dot(dvector *u, dvector *v){
+  assert(u->length == v->length);
+  double dot = 0;
+  int size = u->length;
+
+  for(int i = 0; i<size; i++)
+    dot += u->data[i] * v->data[i];
+
+  return dot;
+}
+
+void vprint(dvector *v){
+  int s = v->length;
+  for(size_t i = 0 ; i<s; i++)
+    printf("%.5f ", v->data[i]); 
 }
 
 
@@ -52,3 +82,5 @@ dvector vector_dif(dvector *u, dvector *v){
 void vector_free(dvector *v){
   free(v->data);
 }
+
+
